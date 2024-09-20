812b145b7e8dd416b3267ddc3a2dc9fe2a74468d
MaheshAwaji <maheshawaji111@gmail.com>
2024-09-20 07:42:11 +0530

    added one printing statement by master

--- src/main/java/com/tyss/gitDemo/basic/Demo.java
+++ src/main/java/com/tyss/gitDemo/basic/Demo.java
@@ -3,5 +3,6 @@ package com.tyss.gitDemo.basic;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Changes done by master");
     }
 }
